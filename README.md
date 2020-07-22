ec2a5f9fe60d49b89adce04219f0af1a7555e927
vikassri <er.vikassri@gmail.com>
2020-07-22 12:46:29 +0530

    updated resize and cropped

--- README.md
+++ README.md
@@ -3,19 +3,21 @@
 Learning OpenCv and Store Code and Notes here
 
 # Images
-```bash 
+```diff 
 VGA = 640x480   (640 pixel in width and 480 pixels in height)
 HD = 1280x720
 FHD = 1920x1080
 4K  = 3840x2160
 ```
 #### Binary Image
-0's and 1's {0 black, 1 white}
+```0's and 1's {0 black, 1 white}```
 
 #### Gray Image
-2ˆ8 = 256 Levels of colors
+```2ˆ8 = 256 Levels of colors```
 
 #### Color Images
+```diff
 Red + Green + Blue = Color
 RGB VGA = 3*640*480 
-RGB HD = 3*1280x720
+RGB HD = 3*1280*720
+```
--- image_resize_cropping.py
+++ image_resize_cropping.py
@@ -4,3 +4,34 @@ import numpy as np
 # Reading the image
 img = cv2.imread("Resources/bird.jpg")
 
+######################  IMAGE RESIZE #################
+# find the size of the images
+print(img.shape)
+"""
+(1333, 2000, 3)  # width, height and channel
+"""
+# resize the image size
+imgResize = cv2.resize(img, (640, 480))
+
+# print the new image size
+print(imgResize.shape)
+"""
+(480, 640, 3)  # width, height and channel
+"""
+# writing this image into new file
+cv2.imwrite("Resources/small_birld.jpg", imgResize)
+
+# showing the small image
+cv2.imshow("Small Size", imgResize)
+
+######################  IMAGE CROPPING #################
+
+ImgCropped = imgResize[0:480, 200:640]  # [height, width]
+cv2.imshow("Cropped Size", ImgCropped)
+
+
+# waiting for the key or 10 secs
+cv2.waitKey(10000)
+
+# Destroy all the windows
+cv2.destroyAllWindows()
